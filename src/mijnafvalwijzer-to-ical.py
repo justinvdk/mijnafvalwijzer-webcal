f41cf697dcf274c3bfdd333ba2d771ff2e153eda
Justin van der Krieken <justin@vdkrieken.com>
2022-12-27 23:07:07 +0100

    Use new PWA endpoints over now broken html scraping

--- src/mijnafvalwijzer-to-ical.py
+++ src/mijnafvalwijzer-to-ical.py
@@ -5,38 +5,55 @@ import re
 import requests
 import sys
 from datetime import datetime, date, timedelta
-from bs4 import BeautifulSoup
 from icalendar import Calendar, Event, Alarm
 
-months = {
-  "januari":   1,
-  "februari":  2,
-  "maart":     3,
-  "april":     4,
-  "mei":       5,
-  "juni":      6,
-  "juli":      7,
-  "augustus":  8,
-  "september": 9,
-  "oktober":   10,
-  "november":  11,
-  "december":  12
-}
+base_url = "https://www.afval3xbeter.nl"
 
-def make_ical(postal_code, housenumber, waste_types):
-  summary_format = os.getenv("MIJNAFVALWIJZER_SUMMARY_FORMAT", "Afval - {description}")
-  description_format = os.getenv("MIJNAFVALWIJZER_DESCRIPTION_FORMAT", "{description}")
-
+def destruct_housenumber(housenumber):
   housenumber_suffix = ""
   housenumber_re = re.search(r"^(\d+)(\D*)$", housenumber)
   if housenumber_re.group():
     housenumber = housenumber_re.group(1)
     housenumber_suffix = housenumber_re.group(2) or ""
 
-  url = "https://www.mijnafvalwijzer.nl/nl/{0}/{1}/{2}".format(postal_code, housenumber, housenumber_suffix)
-  aw_html = requests.get(url)
+  return housenumber, housenumber_suffix
 
-  aw = BeautifulSoup(aw_html.text, "html.parser")
+# https://www.afval3xbeter.nl/adressen/{postcode}:{housenumber}
+def get_address_metadata(postal_code, housenumber):
+  url = f'{base_url}/adressen/{postal_code}:{housenumber}'
+  address_response = requests.get(url)
+  address_response.raise_for_status
+
+  return address_response.json()
+
+# https://www.afval3xbeter.nl/rest/adressen/{bagid}/kalender/2022
+def get_calendar(bagid, year):
+  url = f'{base_url}/rest/adressen/{bagid}/kalender/{year}'
+  calendar_response = requests.get(url)
+  calendar_response.raise_for_status
+
+  return calendar_response.json()
+
+# https://www.afval3xbeter.nl/rest/adressen/{bagid}/afvalstromen
+def get_waste_types_metadata(bagid):
+  url = f'{base_url}/rest/adressen/{bagid}/afvalstromen'
+  waste_types_response = requests.get(url)
+  waste_types_response.raise_for_status
+
+  # cheeky to_dict
+  waste_types = {}
+  for waste_type in waste_types_response.json():
+    waste_types[waste_type['id']] = waste_type
+
+  return waste_types
+
+def make_ical(address_metadata, calendar_data):
+  all_waste_types_metadata = get_waste_types_metadata(address_metadata['bagid'])
+
+  summary_format = os.getenv("MIJNAFVALWIJZER_SUMMARY_FORMAT", "Afval - {description}")
+  description_format = os.getenv("MIJNAFVALWIJZER_DESCRIPTION_FORMAT", "{description}")
+
+  now = datetime.now()
 
   cal = Calendar()
   cal.add("prodid", "-//{0}//NL".format(sys.argv[0]))
@@ -44,51 +61,63 @@ def make_ical(postal_code, housenumber, waste_types):
   cal.add("name", "Afvalkalender")
   cal.add("x-wr-calname", "Afvalkalender")
   cal.add("x-wr-timezone", "Europe/Amsterdam")
-  cal.add("description", aw.title.string)
-  cal.add("url", url)
+  cal.add("description", address_metadata['description'])
+  cal.add("url", "https://www.afval3xbeter.nl/wanneer")
 
   alarm = Alarm()
   alarm.add("action", "DISPLAY")
   alarm.add("trigger", value=timedelta(-1))
 
-  for item in aw.find_all("a", "wasteInfoIcon textDecorationNone"):
-    # Get the waste type from the fragment in the anchors href
-    waste_type = item["href"].replace("#", "").replace("waste-", "")
-    if waste_type == "" or waste_type == "javascript:void(0);":
-      if item.p.has_attr("class"):
-        waste_type = item.p["class"][0]
+  for calendar_item_data in calendar_data:
+    waste_type = calendar_item_data["afvalstroom_id"]
 
-    if not waste_types or waste_type in waste_types:
-      raw_d = re.search("(\w+) (\d+) (\w+)", item.p.text)
-      item_date = date(datetime.now().year, months.get(raw_d.group(3), 0), int(raw_d.group(2)))
-      item_description = item.find("span", {"class": "afvaldescr"}).text
-      item_description = item_description.replace("\,", ",")
+    item_date = datetime.strptime(calendar_item_data['ophaaldatum'], "%Y-%m-%d")
+    item_description = all_waste_types_metadata[calendar_item_data['afvalstroom_id']]['title']
 
-      event = Event()
-      event.add("uid", "{0}-{1}-{2}".format(datetime.now().year, item_date.timetuple().tm_yday, waste_type))
-      event.add("dtstamp", datetime.now())
-      event.add("dtstart", item_date)
-      event.add("dtend", item_date + timedelta(1))
-      event.add("summary", summary_format.format(description = item_description))
-      event.add("description", description_format.format(description = item_description))
-      event.add_component(alarm)
+    event = Event()
+    event.add("uid", "{0}-{1}-{2}".format(item_date.year, item_date.timetuple().tm_yday, waste_type))
+    event.add("dtstamp", now)
+    event.add("dtstart", item_date)
+    event.add("dtend", item_date + timedelta(1))
+    event.add("summary", summary_format.format(description = item_description))
+    event.add("description", description_format.format(description = item_description))
+    event.add_component(alarm)
 
-      cal.add_component(event)
+    cal.add_component(event)
 
   return cal.to_ical().decode("utf-8")
 
 if __name__ == "__main__":
-  if len(sys.argv) < 2:
-    print("Usage {0} <postal code> <house number>".format(sys.argv[0]))
+  if len(sys.argv) != 3:
+    print("Usage {0} postal_code housenumber".format(sys.argv[0]))
     exit(1)
 
   postal_code = sys.argv[1]
   housenumber = sys.argv[2]
-  waste_types = []
 
-  if len(sys.argv) >= 4:
-    waste_types = sys.argv[3].split(",")
+  housenumber, housenumber_suffix = destruct_housenumber(housenumber)
 
-  ical = make_ical(postal_code, housenumber, waste_types)
+  address_metadatas = get_address_metadata(postal_code, housenumber)
+  address_metadatas = list(filter(lambda metadata: metadata['huisletter'] == housenumber_suffix, address_metadatas))
+
+  if len(address_metadatas) != 1:
+    raise f'ambiguous postal_code and housenumber combination. Found {len(address_metadatas)} matches, expecting 1.'
+
+  address_metadata = address_metadatas[0]
+
+  now = datetime.now()
+  current_year = now.year
+
+  calendar = []
+  # Get prev. year in januari.
+  if now.month == 1:
+    calendar = calendar + get_calendar(address_metadata['bagid'], current_year - 1)
+  # Get current year data.
+  calendar = calendar + get_calendar(address_metadata['bagid'], current_year)
+  # Get next year in december.
+  if now.month == 12:
+    calendar = calendar + get_calendar(address_metadata['bagid'], current_year + 1)
+
+  ical = make_ical(address_metadata, calendar)
 
   print(ical)
